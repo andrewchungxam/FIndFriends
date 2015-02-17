2a4d29cb1be675056ef5b6b5f959cc75f92b45a3
Andrew Way <andrew@wayfamily.net>
2015-02-16 21:51:14 -0800

    Android MapsFragment Up and Running

--- XamarinMaps.Android/MainActivity.cs
+++ XamarinMaps.Android/MainActivity.cs
@@ -21,12 +21,12 @@ namespace XamarinMaps.Android {
 
 			// Get our button from the layout resource,
 			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
+			//Button button = FindViewById<Button> (Resource.Id.myButton);
 			
-			button.Click += delegate
-			{
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+//			button.Click += delegate
+//			{
+//				button.Text = string.Format ("{0} clicks!", count++);
+//			};
 		}
 	}
 }
