cd5c0d03dd79a3209a8a11904b735e8b2a01e620
Andrew Chung <andrew.chung@xamarin.com>
2015-02-26 22:49:56 -0800

    Revert 753cc04..f70d18d
    This rolls back to commit 753cc04660d9ee0d5c3e475b619d21bf87ecea24.

---
+++ XamarinMaps/LocationDataPoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SQLite;
+
+namespace XamarinMaps
+{
+
+	[Table("LocationDataPoint")]
+	public class LocationDataPoint
+	{
+
+		public LocationDataPoint ()
+		{
+		}
+
+		[PrimaryKey, AutoIncrement, Column("_id")]
+		public int Id { get; set; }
+
+		public double Longitude { get;  set;}
+		public double Latitude  { get;  set;}
+
+
+		public DateTime aDateTime { get;  set;}
+
+	}
+}
+
